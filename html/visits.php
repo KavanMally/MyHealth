4910d2b72db2b19451e72c1700a9c53ad75894df
KavanMally <kavanmally@gmail.com>
2016-12-06 00:20:41 -0500

    set up visits page

--- html/visits.php
+++ html/visits.php
@@ -17,22 +17,38 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "Cleveland123";
-	$dbname = "test";
+	$dbname = "my_health";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	if($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
 	$input = htmlspecialchars(stripslashes($id));
-	$sql = "SELECT rnum FROM names WHERE pid=".$input;	
-	$result = $conn->query($sql);
+	$sql = 
+	"SELECT vID, vDate
+	 FROM VISITS
+	 WHERE pID = ".$input . ";";	
 	
+	$result = $conn->query($sql);
+
+		
 	if($result->num_rows>0){
+
+		echo "<table style=\"width:50%\" border=\"\">";
+		echo "<tr><th>Visit Dates:</th></tr>";
 		
 		while($row=$result->fetch_assoc()){
-			echo $row["rnum"];
+			
+
+			echo "<tr>";
+			echo "<td><a href=details.php?id=".$row["vID"].">"
+			. $row["vDate"] . "</a></td>";
+			echo "</tr>";
 		}
 	}
+	else echo "0 results<br>";
+
+	echo "</table>";
 
 ?>
 
